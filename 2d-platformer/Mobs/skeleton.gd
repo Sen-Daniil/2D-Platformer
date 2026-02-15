b747abb14a8dc5d92542bd880d111b7f10f4ba56
Daniil <dan.senchihin@yandex.ru>
2026-02-16 00:22:20 +0700

    Beta2

--- 2d-platformer/Mobs/skeleton.gd
+++ 2d-platformer/Mobs/skeleton.gd
@@ -1,58 +1,41 @@
 extends CharacterBody2D
 
+@onready var animPlayer = $AnimationPlayer
 
-var speed = 100
-var chase = false
-@onready var anim = $AnimatedSprite2D
-var alive = true
+enum {
+	IDLE,
+	ATTACK,
+	CHASE
+}
 
-
+var state: int = 0:
+	set(value):
+		state = value
+		match state:
+			IDLE:
+				idle_state()
+			ATTACK:
+				attack_state()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	var player = $"../../Player/Player"
-	var direction = (player.position - self.position).normalized()
-	if alive == true:
-		if chase == false:
-			velocity.x = direction.x * speed
-			anim.play("Run")
-		else:
-			velocity.x = 0
-			anim.play("Idle")
-		if direction.x < 0:
-			$AnimatedSprite2D.flip_h = true
-		else:
-			$AnimatedSprite2D.flip_h = false
-
 
 	move_and_slide()
 
 
-func _on_detector_body_exited(body: Node2D) -> void:
-	if body.name == "Player":
-		chase = true
-
-
-func _on_detector_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		chase = false
-
-
-func _on_death_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		body.velocity.y -= 250
-		death()
-		
-func _on_death_2_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		if alive == true:
-			body.health -= 40
-			death()
-		
-func death ():
-	alive = false
-	anim.play("Death")
-	await anim.animation_finished
-	queue_free()
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	state = ATTACK
+	
+func idle_state():
+	animPlayer.play("Idle")
+	await get_tree().create_timer(1).timeout
+	$AttackDirection/AttackRange/CollisionShape2D.disabled = false
+	state = CHASE
+	
+func attack_state():
+	animPlayer.play("Attack")
+	await animPlayer.animation_finished
+	$AttackDirection/AttackRange/CollisionShape2D.disabled = true
+	state = IDLE
